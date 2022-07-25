91b0732f96191f7ccaa55421e67b9501b34e719c
hargun0360 <hargunsingh0720@gmail.com>
2022-07-25 17:44:19 +0530

    Adding Warranty Page UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 import Generate from './Pages/Generate';
+import Warranty from './Pages/Warranty';
 function App() {
   return (
     <div className="App">
-        <Generate />
+      <Routes>
+        <Route path="/" element={<Generate />} />
+        <Route path="/warranty" element={<Warranty />} />
+      </Routes>
     </div>
   );
 }
